f7957fa695ccd62b3f41a036bcfc010452162fd5
ZoltanMG <zoltanmoragarcia@gmail.com>
2021-04-17 16:06:43 -0500

    [update]: Ahora se imprime cada uno de los nombres de la array lista nombre

--- src/components/ListadoNombres.jsx
+++ src/components/ListadoNombres.jsx
@@ -6,6 +6,12 @@ const ListadoNombres = () => {
     const [nombre, setNombre] = useState('')
     const [listaNombres, setListaNombres] = useState([])
 
+
+    // - addNombre realiza lo siguiente:
+    // evita que la pagina se actualice cuando se envia el formulario,
+    // crea un objeto con un id unico y asigna el nombre regiatrado por el usuario en el formulario,
+    // añade el objeto a la array listaNombre,
+    // vacia el imputo del fomulario diligenciado previamente.
     const addNombre = (e) => {
         e.preventDefault()
         const nuevoNombre = {
@@ -13,6 +19,7 @@ const ListadoNombres = () => {
             nombre: nombre
         }
         setListaNombres([...listaNombres, nuevoNombre])
+        setNombre('')
     }
 
     return (
@@ -21,12 +28,32 @@ const ListadoNombres = () => {
             <div className="row">
                 <div className="col">
                     <h2>Listado de nombres</h2>
+                    <ul className="list-group">
+                        {
+                            listaNombres.map(item =>
+                                <li key={item.id} className="list-group-item">
+                                    {item.nombre}
+                                </li>
+                            )
+                        }
+                    </ul>
                 </div>
                 <div className="col">
                     <h2>Formularios para añadir nombres</h2>
                     <form onSubmit={(e) => addNombre(e)} className="form-group">
-                        <input onChange={(e) => { setNombre(e.target.value) }} className="form-control mb-3" type="text" placeholder="Nombre" />
-                        <input className="btn btn-info btn-block" type="submit" value="Registrar nombre" />
+                        <input
+                            onChange={(e) => { setNombre(e.target.value) }}
+                            className="form-control mb-3"
+                            type="text"
+                            placeholder="Nombre"
+                            value={nombre}
+                            required
+                        />
+                        <input
+                            className="btn btn-info btn-block"
+                            type="submit"
+                            value="Registrar nombre"
+                        />
                     </form>
                 </div>
             </div>
