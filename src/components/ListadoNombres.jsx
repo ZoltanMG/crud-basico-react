86ea9c2c111e4d51437221ad9e1c2fddff10487d
ZoltanMG <zoltanmoragarcia@gmail.com>
2021-04-18 17:06:27 -0500

    [Update]: validación de punstos y comas

--- src/components/ListadoNombres.jsx
+++ src/components/ListadoNombres.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import uniqid from 'uniqid';
 
 const ListadoNombres = () => {
-    const [nombre, setNombre] = useState('')
-    const [listaNombres, setListaNombres] = useState([])
-    const [modoEdicion, setModoEdicion] = useState(false)
-    const [id, setId] = useState('')
-    const [error, setError] = useState(null)
+    const [nombre, setNombre] = useState('');
+    const [listaNombres, setListaNombres] = useState([]);
+    const [modoEdicion, setModoEdicion] = useState(false);
+    const [id, setId] = useState('');
+    const [error, setError] = useState(null);
 
     // - addNombre realiza lo siguiente:
     // evita que la pagina se actualice cuando se envia el formulario,
@@ -14,42 +14,42 @@ const ListadoNombres = () => {
     // añade el objeto a la array listaNombre,
     // vacia el imputo del fomulario diligenciado previamente.
     const addNombre = (e) => {
-        e.preventDefault()
-        if (!nombre.trim()){
-            setError('El campo nombre esta vacio.')
-            return
+        e.preventDefault();
+        if (!nombre.trim()) {
+            setError('El campo nombre esta vacio.');
+            return;
         }
         const nuevoNombre = {
             id: uniqid(),
             nombre: nombre
         }
-        setListaNombres([...listaNombres, nuevoNombre])
-        setNombre('')
-        setError(null)
+        setListaNombres([...listaNombres, nuevoNombre]);
+        setNombre('');
+        setError(null);
     }
 
     // - deleteNombre realiza:
     // crea una nueva array omitiendo el objeto con la id pasada,
     // cambia la nueva array por listaNombres.
     const deleteNombre = (id) => {
-        const nuevaArray = listaNombres.filter(item => item.id !== id)
-        setListaNombres(nuevaArray)
+        const nuevaArray = listaNombres.filter(item => item.id !== id);
+        setListaNombres(nuevaArray);
     }
 
     const editar = (item) => {
-        setModoEdicion(true)
-        setNombre(item.nombre)
-        setId(item.id)
+        setModoEdicion(true);
+        setNombre(item.nombre);
+        setId(item.id);
     }
 
     const editarNombre = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         const nuevoArray = listaNombres.map(item =>
             item.id === id ? { id: id, nombre: nombre } : item
-        )
-        setListaNombres(nuevoArray)
-        setModoEdicion(false)
-        setNombre('')
+        );
+        setListaNombres(nuevoArray);
+        setModoEdicion(false);
+        setNombre('');
     }
 
     return (
@@ -103,15 +103,15 @@ const ListadoNombres = () => {
                             <div className="alert alert-danger">
                                 {error}
                             </div>
-                        ):
-                        (
-                            <div></div>
-                        )
+                        ) :
+                            (
+                                <div></div>
+                            )
                     }
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default ListadoNombres;
